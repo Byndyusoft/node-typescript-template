2e7c56e6aa4c27525834e5976ddf07f2c99ab396
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2021-07-28 08:16:17 +0500

    style: enable bracketSpacing

--- src/__tests__/hello-world.service.spec.ts
+++ src/__tests__/hello-world.service.spec.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {HelloWorldService} from "~/src";
+import { HelloWorldService } from "~/src";
 
 describe("HelloWorldService", () => {
   let helloWorldService: HelloWorldService;
